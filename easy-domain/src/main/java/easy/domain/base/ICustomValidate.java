e7b8b2a2417f9b01ea2d39af176af863a13cf506
lixiaojing10 <lixiaojing3@jd.com>
2021-12-22 16:09:27 +0800

    增加自定义验证接口，用从外部传业务规则类

---
+++ easy-domain/src/main/java/easy/domain/base/ICustomValidate.java
@@ -0,0 +1,11 @@
+package easy.domain.base;
+
+import easy.domain.rules.EntityRule;
+
+/**
+ * @author lixiaojing10
+ * @date 2021/12/22 3:42 下午
+ */
+public interface ICustomValidate<R extends BrokenRuleObject> {
+    Boolean validate(EntityRule<R> rule);
+}
